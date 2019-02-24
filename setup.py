ddf218e1d68398730313a6896a31c323882bc7f4
Hans Erik Heggem <hans.erik.heggem@gmail.com>
2019-02-25 00:49:01 +0100

    upgraded dockerbuildsystem version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.28',  # Required
+    version='0.0.29',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -134,8 +134,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'DockerBuildSystem>=1.1.21',
-        'SwarmManagement>=1.1.32'],  # Optional
+        'DockerBuildSystem>=1.1.22',
+        'SwarmManagement>=1.1.33'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
