1bfdfcdfa166f94a968c0c8b934f78cf9d1efea5
Arild Håkon Olsen <aho@dips.no>
2020-01-08 10:37:03 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.64',  # Required
+    version='0.0.65',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -134,8 +134,8 @@ setup(
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=[
-        'DockerBuildSystem>=1.1.41',
-        'SwarmManagement>=1.1.57'],  # Optional
+        'DockerBuildSystem>=1.1.42',
+        'SwarmManagement>=1.1.58'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
